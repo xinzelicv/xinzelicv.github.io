992c2f22c9112a7fe383188f427cbcf5273e2c62
Powertony102 <77042216+Powertony102@users.noreply.github.com>
2026-03-19 05:49:28 +0000

    Deploying to gh-pages from @ xinzelicv/xinzelicv.github.io@d0726bf2ce055e8d7f53bea973c2c0204efa0854 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum Vitae of Xinze Li, including education, research experience, projects, and selected achievements.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
